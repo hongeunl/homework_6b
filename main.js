f46f82c14989161524589fb69794fc9b7654cc9d
hongeunl <hongeunl@andrew.cmu.edu>
2021-11-07 22:59:38 -0500

    clean

--- main.js
+++ main.js
@@ -28,8 +28,6 @@ function changeTotalPrice(quantity) {
   }
 }
 
-var savedCartArray = JSON.parse(localStorage.getItem("savedCartArray"));
-
 //A new constructor//
 function Roll(name, glaze, subtotal, quant) {
   this.name = name;
@@ -46,14 +44,15 @@ function addToCart(flavor) {
   thisFlavor = flavor;
 
   newItem = new Roll(thisFlavor, thisGlaze, thisSubtotal, thisQuant);
+  var savedCartArray = JSON.parse(localStorage.getItem("savedCartArray") || "[]");
   savedCartArray.push(newItem);
   localStorage.setItem("savedCartArray", JSON.stringify(savedCartArray));
   updateCartNum();
-
 }
 
 //Update the number of items in the cart//
 function updateCartNum() {
+  var savedCartArray = JSON.parse(localStorage.getItem("savedCartArray") || "[]");
   var numItemsCart = savedCartArray.length;
   if (numItemsCart > 0) {
     var cartNum = document.getElementById("cartNumUpdate");
@@ -61,94 +60,37 @@ function updateCartNum() {
   }
 }
 
-function onLoad() {
-  updateCartItems(savedCartArray);
-  for (let i = 0; i < savedCartArray.length; i++) {
-    document.getElementById("delete"+i).addEventListener("click", function() {
-      let newArray = savedCartArray.splice(i,1);
-      localStorage.setItem("savedCartArray", JSON.stringify(newArray));
-      updateCartItems(savedCartArray);
-  });
-}
-}
-
-
-//Update items in the cart page//
-function updateCartItems(savedCartArray) {
-  //Update flavor//
-  var numItemsCart = savedCartArray.length;
-  for (let i = 0; i < numItemsCart; i++) {
-    var itemContainer = document.createElement("div");
-    itemContainer.className = "cart-item-container" + i;
-    let itemNamePlace = document.createElement("h3");
-    let itemName = document.createTextNode(savedCartArray[i].name);
-    itemNamePlace.appendChild(itemName);
-    itemContainer.appendChild(itemNamePlace);
-    document.getElementsByClassName("cart")[0].appendChild(itemContainer);
-
-
-    //Update glaze //
-    let itemGlazePlace = document.createElement("h4");
-    let itemGlaze = document.createTextNode(savedCartArray[i].glaze);
-    itemGlazePlace.appendChild(itemGlaze);
-    itemContainer.appendChild(itemGlazePlace);
-
-    //Update quantity //
-    let itemQuantPlace = document.createElement("select");
-    //Add quantity options //
-    let numOption1 = document.createElement("option");
-    numOption1.value = "1";
-    numOption1.text = "1";
-    let numOption2 = document.createElement("option");
-    numOption2.value = "3";
-    numOption2.text = "3";
-    let numOption3 = document.createElement("option");
-    numOption3.value = "6";
-    numOption3.text = "6";
-    let numOption4 = document.createElement("option");
-    numOption4.value = "12";
-    numOption4.text = "12";
-    //Match the quantity options with what the user selected //
-    let itemQuant = savedCartArray[i].quant;
-    if (itemQuant == numOption1.text) {
-      numOption1.selected = true;
-    }
-    else if (itemQuant == numOption2.text) {
-      numOption2.selected = true;
-    }
-    else if (itemQuant == numOption3.text) {
-      numOption3.selected = true;
-    }
-    else if (itemQuant == numOption4.text) {
-      numOption4.selected = true;
-    }
-    itemQuantPlace.appendChild(numOption1);
-    itemQuantPlace.appendChild(numOption2);
-    itemQuantPlace.appendChild(numOption3);
-    itemQuantPlace.appendChild(numOption4);
-/*  let itemQuant = document.createTextNode(savedCartArray[i].quant);
-    itemQuantPlace.appendChild(itemQuant); */
-    itemContainer.appendChild(itemQuantPlace);
-
-    //Create x button //
-    let xButtonPlace = document.createElement("button");
-    xButtonPlace.className = "deleteButton";
-    xButtonPlace.id = "delete" + i;
-    // let xButton = document.createTextNode("X");
-    xButtonPlace.innerHTML = "X";
-    // xButtonPlace.appendChild(xButton);
-    itemContainer.appendChild(xButtonPlace);
-  
-
-    //Update subtotal //
-    let itemSubtotalPlace = document.createElement("h5");
-    let itemSubtotal = document.createTextNode(savedCartArray[i].subtotal);
-    itemQuantPlace.appendChild(itemSubtotal);
-    itemContainer.appendChild(itemSubtotalPlace);
-
+//Update & display items on the cart page//
+function updateCartItems(){
+  var savedCartArray = JSON.parse(localStorage.getItem("savedCartArray") || "[]");
+  var cartContainer = document.getElementById("cartfeed");
+  if (savedCartArray.length == 0) {
+    document.getElementById("cartfeed").innerHTML = "The cart is empty!";
+  }
+  for (let i=0; i<savedCartArray.length; i++) {
+    cartContainer.innerHTML += `
+      <div class="cart-item-container">
+        <h2>`+ "x " + savedCartArray[i].quant+`</h2>
+        <h3>`+ savedCartArray[i].name+`</h3>
+        <h4>w/ `+ savedCartArray[i].glaze+`</h4>
+        <h5>`+ savedCartArray[i].subtotal+`</h5>
+        <h6 onclick="removeItemFromCart(`+i+`)">x</h6>
+      </div>
+    `;
   }
 }
+updateCartItems();
 
+//Remove items in the cart with the 'x'//
+function removeItemFromCart(position){
+  var savedCartArray = JSON.parse(localStorage.getItem("savedCartArray") || "[]");
+  savedCartArray.splice(position,1);
+  localStorage.setItem("savedCartArray", JSON.stringify(savedCartArray));
+  location.reload();
+}
 
-
-
+//Get rid of all items in the cart//
+function clearFeed() {
+  localStorage.setItem("savedCartArray", JSON.stringify([]));
+  location.reload();
+}
